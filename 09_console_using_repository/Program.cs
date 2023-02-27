bfc1795fcdf04e3ea7d33ecdf5393cfc7758d835
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-02-27 12:52:21 +0200

    add pattern repository

---
+++ 09_console_using_repository/Program.cs
@@ -0,0 +1,22 @@
+﻿using _05_EF_example;
+using _05_EF_example.Entities;
+using data_access_entity.Repositories;
+using System;
+using System.Linq;
+
+namespace _09_console_using_repository
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IRepository<Flight> repository = new Repository<Flight>(new AirplanesDbContext());
+            foreach (var f in repository.GetAll())
+            {
+                Console.WriteLine($"Flight {f.Number } : from {f.DepartureCity} " +
+                    $"to { f.ArrivalCity}");
+            }
+
+        }
+    }
+}
---
+++ data_access_entity/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using _05_EF_example.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_access_entity.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        //CRUD - creade , read, update , delete
+        IEnumerable<TEntity> GetAll();
+        TEntity GetByID(int Id);
+        void Insert(TEntity entity);
+        void Delete(int ID);
+        void UpdateStudent(TEntity entity);
+        void Save();
+    }
+}
